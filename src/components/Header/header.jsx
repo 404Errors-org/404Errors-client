74e3638fdf8ece0288adfa5a4b40ea70913d5f1a
nezeracuk <skibickignazar@gmail.com>
2025-04-13 17:01:11 +0300

    header fix, console errors fix, start/end pins fixed

--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -1,26 +1,30 @@
 import React from "react";
 import { HeaderContainer, LoginBtn, NavItem, Link, NavList, Wrapper } from "./header.styled";
-import Map from "../map/map";
 import { useAuth } from "../../context/authContext";
 
 const Header = ({ onLocationClick }) => {
-    const { user, isLoggedIn, logout } = useAuth();
-    return(
-      <>
-        <HeaderContainer>
-            <Wrapper>
-                <img src="/icons/logo.png" alt="logo" width={65} height={65}></img>
-                <Link onClick={onLocationClick} marginLeft='50px'>Локації</Link>
-            </Wrapper>
-            <NavList>
-                {isLoggedIn ? <Link onClick={logout}>{user.username}</Link> :
-                <NavItem><LoginBtn to='/login'>Увійти</LoginBtn></NavItem>}
-            </NavList>
-        </HeaderContainer>
-
-        {/* <Map /> */}
-      </>
-    )
-}
+  const { user, isLoggedIn, logout } = useAuth();
+  return (
+    <>
+      <HeaderContainer>
+        <Wrapper>
+          <img src="/icons/logo.png" alt="logo" width={65} height={65}></img>
+          <Link onClick={onLocationClick} marginLeft="50px">
+            Локації
+          </Link>
+        </Wrapper>
+        <NavList>
+          {isLoggedIn ? (
+            <Link onClick={logout}>{user.username}</Link>
+          ) : (
+            <NavItem>
+              <LoginBtn to="/login">Увійти</LoginBtn>
+            </NavItem>
+          )}
+        </NavList>
+      </HeaderContainer>
+    </>
+  );
+};
 
 export default Header;
